5c1f4bebbda49268fca4d6d3cd26bf925f9fd1e7
jlarm <117611704+jlarm@users.noreply.github.com>
2025-08-29 14:05:37 -0500

    Season migration and model

---
+++ app/Models/Season.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Season extends Model
+{
+    use SoftDeletes;
+
+    protected $fillable = [
+        'uuid',
+        'name',
+    ];
+
+    public function scopeOrderBySeasonAndYear(Builder $query): Builder
+    {
+        return $query->orderByRaw('CAST(SUBSTR(name, -4) AS INTEGER)')
+            ->orderByRaw("
+            CASE
+                WHEN INSTR(LOWER(name), 'spring') > 0 THEN 1
+                WHEN INSTR(LOWER(name), 'summer') > 0 THEN 2
+                WHEN INSTR(LOWER(name), 'fall') > 0 THEN 3
+                WHEN INSTR(LOWER(name), 'winter') > 0 THEN 4
+                ELSE 5
+            END
+        ");
+    }
+
+    public function teams(): HasMany
+    {
+        return $this->hasMany(Team::class);
+    }
+
+    protected function casts(): array
+    {
+        return [
+            'uuid' => 'string',
+        ];
+    }
+}
